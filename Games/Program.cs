cbcf08031ce23e3c2609ea58473f867d660008ff
Олег Султанов <oleg.sultanov933@gmail.com>
2020-05-25 16:17:12 +0700

    Добавлен вывод нолика, он может у вас не работать.

--- Games/Program.cs
+++ Games/Program.cs
@@ -10,24 +10,29 @@ namespace Games
     {
         static void Main(string[] args)
         {
+          
+           
+            bool beskonehno = true;
             Random random = new Random();
-            int xc = random.Next(1, 3);
+            int xc = random.Next(1, 4);
 
-            int yc = random.Next(1, 3);
+            int yc = random.Next(1, 4);
             char krest = 'х';
             char nuli = '0';
             char minus = '-';
+            
             int[,] strokasimwol = new int[3, 3];
             // вывод поля 3 на 3 ---
             //                   ---
             //                   ---
-            for (int i = 0; i < strokasimwol.GetLength(0); i++)
+            for (int stroka = 0; stroka < strokasimwol.GetLength(0); stroka++)
             {
-                for (int j = 0; j < strokasimwol.GetLength(1); j++)
+                for (int simvol = 0; simvol < strokasimwol.GetLength(1); simvol++)
                 {
                     Console.Write(minus);
                 }
                 Console.WriteLine();
+               
             }
 
             while (true) // бесконечный цикл
@@ -41,8 +46,27 @@ namespace Games
                 Console.CursorTop = y - 1;
                 Console.CursorLeft = x - 1;
                 Console.WriteLine(krest);
+                beskonehno = true;
                 // Нолик
+                while (beskonehno)
+                {
+                    xc = random.Next(1, 4);
+                    yc = random.Next(1, 4);
+                    if (strokasimwol[xc,yc] == minus)
+                    { 
+                        Console.CursorTop = xc - 1;
+                        Console.CursorLeft = yc - 1;
+                        Console.WriteLine(nuli);
+                        beskonehno = false;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
             }
+            
+            
         }
     }
 }
