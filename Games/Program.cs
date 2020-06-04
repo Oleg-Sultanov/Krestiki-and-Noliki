746d99011cd1fe2b49dc26485b7d5e37569d2194
Олег Султанов <oleg.sultanov933@gmail.com>
2020-06-04 15:35:30 +0700

    Добавлен окончательно вывод нолика :) теперь все работает ХОРОШО

--- Games/Program.cs
+++ Games/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,63 +11,96 @@ namespace Games
     {
         static void Main(string[] args)
         {
-          
-           
-            bool beskonehno = true;
-            Random random = new Random();
-            int xc = random.Next(1, 4);
+            char[,] pole = new char[3, 3] { { '-', '-', '-' }, { '-', '-', '-' }, { '-', '-', '-' } };
+            Random rnd = new Random();
+            bool beskonechno = true;
 
-            int yc = random.Next(1, 4);
-            char krest = 'х';
-            char nuli = '0';
-            char minus = '-';
+
+
+
+
             
-            int[,] strokasimwol = new int[3, 3];
-            // вывод поля 3 на 3 ---
-            //                   ---
-            //                   ---
-            for (int stroka = 0; stroka < strokasimwol.GetLength(0); stroka++)
-            {
-                for (int simvol = 0; simvol < strokasimwol.GetLength(1); simvol++)
-                {
-                    Console.Write(minus);
-                }
-                Console.WriteLine();
-               
-            }
 
-            while (true) // бесконечный цикл
+
+
+
+            while (beskonechno) // цикл
             {
+
+                // вывод поля 3 на 3 --- потом изменится
+                //                   ---
+                //                   ---
+                for (int stroka = 0; stroka < pole.GetLength(0); stroka++)
+                {
+                    for (int simvol = 0; simvol < pole.GetLength(1); simvol++)
+                    {
+                        Console.Write(pole[stroka, simvol]);
+                    }
+                    Console.WriteLine();
+
+                }
+
+
+
                 // Крестик
                 // выводит крестик где укажет пользователь
                 Console.CursorTop = 3;
                 Console.WriteLine("Введите координаты крестика");
                 int x = Convert.ToInt32(Console.ReadLine());
                 int y = Convert.ToInt32(Console.ReadLine());
-                Console.CursorTop = y - 1;
-                Console.CursorLeft = x - 1;
-                Console.WriteLine(krest);
-                beskonehno = true;
-                // Нолик
-                while (beskonehno)
+                if (pole[x, y] == '-')
                 {
-                    xc = random.Next(1, 4);
-                    yc = random.Next(1, 4);
-                    if (strokasimwol[xc,yc] == minus)
-                    { 
-                        Console.CursorTop = xc - 1;
-                        Console.CursorLeft = yc - 1;
-                        Console.WriteLine(nuli);
-                        beskonehno = false;
-                    }
-                    else
+                    if (pole[x, y] == '-')
                     {
-                        continue;
+                        if (pole[x, y] == '-')
+                        {
+                            pole[x, y] = 'x';
+                        }
+                        else if (pole[x, y] != '-')
+                        {
+                            beskonechno = false;
+                        }
                     }
                 }
+                else if (x > 2)
+                {
+                    beskonechno = false;
+                }
+
+
+
+
+                // вывод Нолика рандомно
+
+                
+                    int xc = rnd.Next(0, 3);
+                    int yc = rnd.Next(0, 3);
+                    if (pole[xc, yc] == '-')
+                    {
+                        if (pole[xc, yc] == '-')
+                        {
+                            if (pole[xc, yc] == '-')
+                            {
+                                pole[xc, yc] = '0';
+                            }
+                        }
+                    }
+                    // вывод поля когда вывелся x и 0
+                    for (int stroka = 0; stroka < pole.GetLength(0); stroka++)
+                    {
+                        for (int simvol = 0; simvol < pole.GetLength(1); simvol++)
+                        {
+                            Console.Write(pole[stroka, simvol]);
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.Clear();
+
+                }
+                Console.ReadKey();
             }
-            
-            
+
         }
     }
-}
+
+    
---
+++ sdsdsdssd/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sdsdsdssd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] pole = new char[3, 3] { { '-', '-', '-' }, { '-', '-', '-' }, { '-', '-', '-' } };
+            bool d = true;
+            while (d)
+            {
+                for (int i = 0; i < pole.GetLength(0); i++)
+                {
+                    for (int j = 0; j < pole.GetLength(1); j++)
+                    {
+                        Console.Write(pole[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("Введите координату х");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите координату y");
+                int y = Convert.ToInt32(Console.ReadLine());
+                if (pole[x, y] == '-')
+                {
+                    if (pole[x, y] == '-')
+                    {
+                        if (pole[x, y] == '-')
+                        {
+                            pole[x, y] = 'x';
+                        }
+                        else if (pole[x, y] != '-')
+                        {
+                            d = false;
+                        }
+                    }
+                }
+                else if (x > 1)
+                {
+                    d = false;
+                }
+
+                Random rnd = new Random();
+                int O = rnd.Next(0, 2);
+                int O2 = rnd.Next(0, 2);
+                if (pole[O, O2] == '-')
+                {
+                    if (pole[O, O2] == '-')
+                    {
+                        if (pole[O, O2] == '-')
+                        {
+                            pole[O, O2] = '0';
+                        }
+                    }
+                }
+
+                for (int i = 0; i < pole.GetLength(0); i++)
+                {
+                    for (int j = 0; j < pole.GetLength(1); j++)
+                    {
+                        Console.Write(pole[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+                Console.Clear();
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
